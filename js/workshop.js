3785b258455af2951a0901d2912a298aa478501b
Shubham Shukla <shubhamshukla774@gmail.com>
2020-10-14 14:52:42 +0530

    fetched data from api and render in browser

--- js/workshop.js
+++ js/workshop.js
@@ -1,34 +1,43 @@
 (function() {
     let workshopList = document.querySelector('#workshop-list');
 
-    function addWorkshop() {
+    function fetchWorkshops() {
+        return axios.get('https://workshops-server.herokuapp.com/workshops') 
+                    .then(response => response.data)
+                    .catch(console.log('Error Occur'));
+    };
+
+    function addWorkshop( workshop ) {
         workshopList.innerHTML += `
         <li class="col">
                     <div class="card my-3">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/AngularJS_logo.svg/2000px-AngularJS_logo.svg.png" 
+                        <img src=${workshop.imageUrl} 
                         alt="Angular Img" class="img-fluid">
-                        <h2 class="text-center">Angular JS Bootcamp</h2>
+                        <h2 class="text-center">${workshop.name}</h2>
                         <div>
-                            <small><span class="date">2019-01-01T04:00:00.000Z</span></small> - 
-                            <small><span class="date">2019-01-03T08:00:00.000Z</span></small>
+                            <small><span class="date">${workshop.startDate}</span></small> - 
+                            <small><span class="date">2${workshop.endDate}</span></small>
                         </div>
                         <div>
-                            <small>9:30 am - 1:30 pm</small>
+                            <small>${workshop.time}</small>
                         </div>
                         <div class="my-3">
-                            <span>Tata Elxsi, Prestige Shantiniketan</span>,
-                            <span>Bangalore</span>
-                            <span>Karnataka</span>
+                            <span>${workshop.location.address}</span>,
+                            <span>${workshop.location.city}</span>,
+                            <span>${workshop.location.state}</span>
                         </div>
-                        <div>
-                            <p><strong>AngularJS</strong> (also written as <strong>Angular.js</strong>) is a JavaScript-based open-source front-end web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications.</p>
-                            <p>It aims to simplify both the development and the testing of such applications by providing a framework for client-side model–view–controller (MVC) and model–view–viewmodel (MVVM) architectures, along with components commonly used in rich Internet applications. (This flexibility has led to the acronym MVW, which stands for "model-view-whatever" and may also encompass model–view–presenter and model–view–adapter.)</p>
-                        </div>
+                        <div>${workshop.description}</div>
                     </div>
                 </li>`
     };
 
-    for(let i=0;i<6;i++) {
-        addWorkshop();
+    function addWorkshops( workshops ) {
+        workshops.forEach ( workshop => addWorkshop ( workshop ));
     }
+
+    function init() {
+        fetchWorkshops().then(worshops => addWorkshops(worshops));
+    }
+
+    init();
 })();
